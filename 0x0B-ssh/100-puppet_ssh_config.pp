1942d4b55e60ab55112a29a2a8feb95d58d1c77e
Michael-Maina <michaelmaina02@gmail.com>
2023-03-27 01:10:46 +0300

    Bug fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # Edits the SSH configuration file
-$str = "Host *
-     IdentityFile ~/.ssh/school
-     IdentitiesOnly yes
-     "
+file_line { 'no password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
+}
 
-file { '/home/root/.ssh/config':
-  ensure  => present,
-  path    => '/home/root/.ssh/',
-  content => $str
+file_line { 'private key at ~/.ssh/school':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
