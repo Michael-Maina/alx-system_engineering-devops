a49f12dd06a16fa6e4cc00b59e8cc7ecaacc01f6
Michael-Maina <michaelmaina02@gmail.com>
2023-05-09 16:38:44 +0300

    Added task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to return a list of the titles of all hot posts
+for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], params={}):
+    """
+    Returns a list of titles of all hot posts of a subreddit
+    """
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    header = {"User-Agent": "MyUser"}
+
+    response = requests.get(url, allow_redirects=False,
+                            headers=header, params=params)
+    data = response.json().get("data")
+    after = data.get("after")
+    before = data.get("before")
+
+    if response.status_code == 404:
+        return(None)
+    if response.status_code == 200:
+        req = data.get("children")
+        for item in req:
+            hot_list.append(item.get("data").get("title"))
+
+    if after is None:
+        return(hot_list)
+
+    return recurse(subreddit, hot_list,
+                   params={"after": after, "before": before})
