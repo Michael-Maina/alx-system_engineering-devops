b7fa9d0eea95f440d1ea34fd248b8429e860e99f
Michael-Maina <michaelmaina02@gmail.com>
2023-05-18 06:38:05 +0300

    Error handling

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
 # Change file limit for nonlogin user.
 exec { 'file_limit':
-  command  => "sed -i 's/ULIMIT=\"-n \d+\"/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
+  command  => "sed -i 's/15/4096/2g' /etc/default/nginx",
   provider => 'shell'
+}
+
+exec { 'restart_NGINX':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+  require  => Exec['file_limit']
 }
